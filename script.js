cc41100ffcd3ac7066315819388b4faac9818f66
Vishnupriya1627 <mamillapallivishnupriya@gmail.com>
2026-06-01 18:01:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong", "stop"];
+
+let currentAudio = null;
+
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(`sounds/${sounds[index]}.mp3`);
+    console.log(`clicked sound is sounds/${sounds[index]}.mp3`)
+    currentAudio.play();
+  });
+});
+
+document.querySelector(".stop").addEventListener("click", () => {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
